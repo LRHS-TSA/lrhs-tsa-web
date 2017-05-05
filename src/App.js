e1829c6ea191a910f77635193015543ac527d619
Christopher Miller <admin@mctherealm.net>
2017-05-05 13:33:39 -0400

    Implemented router and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import { BrowserRouter as Router,
+      Route,
+      Link } from 'react-router-dom';
+
+
+
+import TopNav from './views/layouts/NavBar'
+
+import Home from './views/Home'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+      <div>
+        <TopNav/>
+          <Route exact path="/" component={Home}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Home from './views/Home'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 
 ReactDOM.render(
-  <App />,
+    <App />,
   document.getElementById('root')
 );
